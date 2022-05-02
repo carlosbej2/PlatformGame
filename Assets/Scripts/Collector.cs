9ae49a6862fdff15b7c7e538ce1920c01968adc5
Carlos Bayón <carlosupv86@gmail.com>
2022-05-02 18:02:57 +0200

    Detalles finales implementados!!!!!

--- Assets/Scripts/Collector.cs
+++ Assets/Scripts/Collector.cs
@@ -2,11 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Collector : MonoBehaviour
 {
-     int coinsCollected = 0;
-   
+    int coinsCollected = 0;
+    bool hasAllCoins = false;
+    private Scene scene;
+
+   /* void Start()
+    {
+        scene = SceneManager.GetActiveScene().name;
+    }
+   */
     [SerializeField] Text coinsCollectedText;
     [SerializeField] AudioSource coinCollectedS;
 
@@ -17,7 +25,22 @@ public class Collector : MonoBehaviour
             coinCollectedS.Play();
             coinsCollected++;
             coinsCollectedText.text = "Coins collected: " + coinsCollected;
+
+            if(coinsCollected==5){
+                hasAllCoins = true;
+                Debug.Log("TODAS MONDEPAS");
+            }
             
+        }
+
+    
+    }
+
+    void Update(){
+
+        if(hasAllCoins && SceneManager.GetActiveScene().name=="Level1"){
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            hasAllCoins = false;
         }
     }
 
